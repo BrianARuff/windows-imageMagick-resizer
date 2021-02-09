ecfc4e8cc17a655ab3c9343ebb905723f733b560
Brian Ruff <briananthonyruff@gmail.com>
2021-02-08 19:59:23 -0500

    removed unused js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const exec = require("child_process").exec;
 
 function resizeImages(startingPoint="./", oldImagePath=[__dirname], newImagePath=[__dirname], desiredImageOutputSize=["32x32"]) {
   fs.readdir(startingPoint, (err, files) => {
-    const x = files;
     for(let i = 0; i < oldImagePath.length; i++) {
       exec(`magick ${oldImagePath[i]} -resize ${desiredImageOutputSize[i]} ${newImagePath[i]}`, (error, stdout, stderr) => {
         if (error) {
