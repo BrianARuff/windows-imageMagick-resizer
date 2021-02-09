bd0fb1bc20791ba1d0a32b0e521ce56e2920284d
Brian Ruff <briananthonyruff@gmail.com>
2021-02-08 20:02:18 -0500

    reorder logic for errors

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const exec = require("child_process").exec;
 
 function resizeImages(startingPoint="./", oldImagePath=[__dirname], newImagePath=[__dirname], desiredImageOutputSize=["32x32"]) {
   fs.readdir(startingPoint, (err, files) => {
-    for(let i = 0; i < oldImagePath.length; i++) {
-      exec(`magick ${oldImagePath[i]} -resize ${desiredImageOutputSize[i]} ${newImagePath[i]}`, (error, stdout, stderr) => {
-        if (error) {
-          throw new Error(error);
-        } else {
-          if (err) {
-            throw new Error(err);
+    if (err) {
+      throw new Error(err);
+    } else {
+      for(let i = 0; i < oldImagePath.length; i++) {
+        exec(`magick ${oldImagePath[i]} -resize ${desiredImageOutputSize[i]} ${newImagePath[i]}`), (error, stdout, stderr) => {
+          if (error) {
+            throw new Error(error);
           } else {
             console.log("Files resized and renamed");
           }
         }
-      });
+      }
     }
-  })
+  });
 }
 
 module.exports = resizeImages;
